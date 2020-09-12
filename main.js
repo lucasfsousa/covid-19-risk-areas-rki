243b3c27a2ee1c0bc8c6f0e993b029f72cb7ce55
Lucas Felix de Sousa <lucasfsousa@gmail.com>
2020-09-12 10:38:03 +0200

    update project automatically every hour

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const attributions =
     <h3>This is an opensource project that shows international risk areas according to <a target="_blank" href="https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Risikogebiete_neu.html">Robert Koch-Institut© (RKI)</a></h3>
     <p>This website is not related by any means with the German government and the information here can contain errors or be outdated.<br />
     Please check <a target="_blank" href="https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Risikogebiete_neu.html">RKI</a> website for updated information.</p>
-    <small>This website will be automatically updated every 4 hours, Last update: <code>${lastUpdate} CET</code></small>
+    <small>This website will be automatically updated every hour, Last update: <code>${lastUpdate} CET</code></small>
     <p>If you want to contribute, please check <a target="_blank" href="https://github.com/lucasfsousa/covid-19-risk-areas-rki">Github repository</a>.</p>
     `;
 
