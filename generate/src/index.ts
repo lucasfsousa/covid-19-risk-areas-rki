9353d46365697c552ca60926e246ab9557e192e3
Lucas Felix de Sousa <lucasfsousa@gmail.com>
2020-09-12 12:13:29 +0200

    move javascript files to typescript

--- generate/src/index.ts
+++ generate/src/index.ts
@@ -5,9 +5,9 @@ const moment = require('moment-timezone');
 getRiskAreas().then(areas => {
     const current = moment().tz("Europe/Berlin").format('DD/MM/YYYY HH:mm');
 
-    const s = `export const lastUpdate = '${current}';\nexport const riskAreas = ${JSON.stringify(areas)}`;
+    const s = `export const lastUpdate = '${current}';\nexport const riskAreas = ${JSON.stringify(areas)};`;
 
-    fs.writeFile("../risk-areas.js", s, function(err: any) {
+    fs.writeFile("../src/risk-areas.ts", s, function(err: any) {
         if(err) {
             throw err;
         }
